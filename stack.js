06b89eaff87fa304a6a3097631da6e5557cf0291
duarbdhks <duarbdhks@gmail.com>
2021-04-10 08:02:32 +0900

    stack 스터디

---
+++ stack.js
@@ -0,0 +1,15 @@
+/*
+스택
+택배 상하차: 물건을 넣고, 꺼낼땐 마지막 쌓은 물건부터 꺼냄.
+- 시간복잡도: O(1)
+ */
+let stack = [];
+
+stack.push(7)
+stack.push(5)
+stack.push(4)
+stack.pop()
+stack.push(6)
+stack.pop()
+
+console.log(stack)
