48c1fbea744c38b914f2c4fe0d29f2232eaf7ea2
duarbdhks <duarbdhks@gmail.com>
2021-04-10 08:13:46 +0900

    queue 출력

--- queue.js
+++ queue.js
@@ -1,6 +1,19 @@
 /*
 큐
 은행 창구: 번호표 먼저 뽑은 사람부터 처리해줌
- */
+ - 시간복잡도: O(1)
+*/
+let queue = [];
+queue.push(7);
+queue.push(5);
+queue.push(4);
+queue.shift();
+queue.push(6);
+queue.shift();
 
+console.log(queue)
+while (queue.length !== 0) {
+    console.log(queue[0])
+    queue.shift()
+}
 
